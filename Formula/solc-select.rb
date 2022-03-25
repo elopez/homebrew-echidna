dad1023ad2bedc976d081a8f229ab73c925b81a2
Emilio López <2642849+elopez@users.noreply.github.com>
2022-03-25 22:48:31 +0000

    solc-select: add 0.2.1 bottle.

--- Formula/solc-select.rb
+++ Formula/solc-select.rb
@@ -8,6 +8,13 @@ class SolcSelect < Formula
   license "AGPL-3.0-only"
   head "https://github.com/crytic/solc-select.git", branch: "dev"
 
+  bottle do
+    root_url "https://github.com/elopez/homebrew-echidna/releases/download/solc-select-0.2.1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "2db9f8a323ecab57f58c4606229247434a1e3501b7a5289ba03e7e7d7f9f85f4"
+    sha256 cellar: :any_skip_relocation, catalina:     "8541aceee98ac0ee743134cf3a91003f4bc80947a48f2347022bef6d326cc545"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "03f3ec3a1e11b46c8b8d1188d3a59006a5ee80d8e67bd17043d2f7655397c145"
+  end
+
   depends_on "python"
 
   def install
