eb8fa744b90467ddcc916c926fde778a4efe073c
Emilio López <2642849+elopez@users.noreply.github.com>
2022-08-19 12:13:13 +0000

    libff: add 0.2.1 bottle.

--- Formula/libff.rb
+++ Formula/libff.rb
@@ -7,6 +7,14 @@ class Libff < Formula
       revision: "5835b8c59d4029249645cf551f417608c48f2770"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/elopez/homebrew-echidna/releases/download/libff-0.2.1"
+    sha256 cellar: :any,                 monterey:     "246a33f32ea07c7399f1874ad7aa1a7565fd50000a7a11e3dbd8cf31c8d1c009"
+    sha256 cellar: :any,                 big_sur:      "f8c75e42e64ec079c9303e937f35370fe2ecf8c92314c9c688c96ca58072dbe0"
+    sha256 cellar: :any,                 catalina:     "f609dca27228fe3cc686ef61167d9a5055cd2a86b79e54bfcd208d2087be6aa5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e6a7efa6f802daf90fb7cbc6cf8f6d80798726f8de21b7d4eb182af2f29fd82c"
+  end
+
   depends_on "cmake" => :build
   depends_on "openssl@1.1" => :build
 
